6c4891a0c73c8bf89bdc87b59299ba66ad6ecc25
Nguyen Viet Bach <nvbach91@outlook.com>
2019-07-15 15:18:44 +0200

    Updates

---
+++ precache-manifest.3a1cef33882b05816b093021385c746a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25065da1e4df52731494b8b0665c0b8",
+    "url": "/iga-hybrid/index.html"
+  },
+  {
+    "revision": "72150152f3b60ee963da",
+    "url": "/iga-hybrid/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "2dcf850e86b5f302f785",
+    "url": "/iga-hybrid/static/js/2.878a6613.chunk.js"
+  },
+  {
+    "revision": "72150152f3b60ee963da",
+    "url": "/iga-hybrid/static/js/main.42d4da11.chunk.js"
+  },
+  {
+    "revision": "017c84058a9be0d0ac36",
+    "url": "/iga-hybrid/static/js/runtime~main.314416a5.js"
+  }
+]);
